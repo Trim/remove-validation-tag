e2b5f2b0dcf56e587f7f1c0f864e9d89c26396b4
Adrien Dorsaz <adrien@adorsaz.ch>
2024-04-11 22:12:00 +0200

    Update remove-disable-background.js

--- firefox/remove-disable-background.js
+++ firefox/remove-disable-background.js
@@ -1,19 +1,32 @@
 
 // create context menu button
 browser.menus.create({
-    id: "remove-disable",
-    title: "Remove disabled HTML attribute",
+    id: "remove-validation-tag-disable",
+    title: "Enable the field",
+    contexts: ["all"]
+});
+
+browser.menus.create({
+    id: "remove-validation-tag-required",
+    title: "Allow empty values",
     contexts: ["all"]
 });
 
 // listen for context menu click and check if it's our menu item
 browser.menus.onClicked.addListener(info => {
-    if(info.menuItemId === "remove-disable"){
-        // Call remove-disable.js function and, pass the id of the right clicked element
-        // for the script to have the tab context and get the element,
-        // this script is a background script so it's not in the right context.
+    /*
+     * Execute the RemoveValidationTag function in the active tab context
+     * to be able to modify HTML elements.
+     */
+    if(info.menuItemId === "remove-validation-tag-disable"){
         browser.tabs.executeScript({
-            code: `RemoveDisable(${info.targetElementId});`,
+            code: `RemoveValidationTag('disabled', ${info.targetElementId});`,
+        });
+    }
+    
+    if(info.menuItemId === "remove-validation-tag-required"){
+        browser.tabs.executeScript({
+            code: `RemoveValidationTag('required', ${info.targetElementId});`,
         });
     }
 });
