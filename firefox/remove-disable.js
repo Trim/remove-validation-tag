13c81e27013d7e62343d33603b95aded98c45973
Adrien Dorsaz <adrien@adorsaz.ch>
2024-04-11 22:14:59 +0200

    Update remove-disable.js

--- firefox/remove-disable.js
+++ firefox/remove-disable.js
@@ -1,20 +1,20 @@
-function RemoveDisable(tid){ 
+function RemoveValidationTag(attribute, tid){ 
     let node = browser.menus.getTargetElement(tid); // Target ID
 
     // start from parent because for some radio buttons the radio is a sibling
-    recursiveRemoveDisable(node.parentNode);
+    recusriveRemoveTag(attribute, node.parentNode);
 }
 
-function recursiveRemoveDisable(node){
+function recusriveRemoveTag(attribute, node){
 
     // if this node has the attribute, remove it
-    if(node.hasAttribute("disabled")){
-        node.removeAttribute("disabled");
+    if(node.hasAttribute(attribute)){
+        node.removeAttribute(attribute);
     }
 
     // call this function for each of the nested elements
     // in PEGA the buttons are usually nested inside other elements
     for(var i=0; i < node.children.length; i++){
-        recursiveRemoveDisable(node.children[i]);
+        recusriveRemoveTag(attribute, node.children[i]);
     }
 }
